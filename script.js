b3d51fe56f167166fe690121fd09c8cfa62e8116
BassMasterTT <tothtibi22@gmail.com>
2021-01-27 19:53:27 +0800

    refresh splash score working

--- script.js
+++ script.js
@@ -37,14 +37,17 @@ let timePlayed = 0;
 let baseTime = 0;
 let penaltyTime = 0;
 let finalTime = 0;
-let finalTimeDisplay = "0.0s";
+let finalTimeDisplay = "0.0";
 
 // Scroll
 let valueY = 0;
 
 // Refresh Splash Page Best Scores
 function bestScoresToDOM() {
-  
+  bestScores.forEach((bestScore, index) => {
+    const bestScoreEl = bestScore;
+    bestScoreEl.textContent = `${bestScoreArray[index].bestScore}s`;
+  });
 }
 
 // Scheck Local Storage for Best Scores, set bestScoreArray
@@ -60,6 +63,7 @@ function getSavedBestScores() {
     ];
     localStorage.setItem("bestScores", JSON.stringify(bestScoreArray));
   }
+  bestScoresToDOM();
 }
 
 // Reset Game
